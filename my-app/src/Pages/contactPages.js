f4dea37a955eccd8d5770cdec36fe661eae40ef8
Mhsnarsln09 <mhsnarsln09@gmail.com>
2022-08-02 13:39:53 +0300

    contactpage

--- my-app/src/Pages/contactPages.js
+++ my-app/src/Pages/contactPages.js
@@ -29,7 +29,7 @@ export function ContactPages() {
                                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                                <label htmlFor="exampleFormControlTextarea1" className="form-label">What we can help yo with?</label>
                                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
                             </div>
 
